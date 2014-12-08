7e067668eac68affdfca737b4433687de664ac3c
mr2day <beres.nick@gmail.com>
2014-12-08 22:05:42 +0200

    added azure db

--- db.js
+++ db.js
@@ -1,6 +1,7 @@
 var Mongoose = require('mongoose');
 
-var dbURL = 'mongodb://golemuser:Gol3m007@ds061360.mongolab.com:61360/heroku_app32285548' || 'localhost';
+//var dbURL = 'mongodb://golemuser:Gol3m007@ds061360.mongolab.com:61360/heroku_app32285548' || 'localhost';
+var dbURL = 'mongodb://golemserver.cloudapp.net' || 'localhost';
 
 // connect to the db
 Mongoose.connect(dbURL+'/find-my-car-one');
